4553d1774d2c896f6de103c5ba2bee8837474a1d
Eduardo Graziano <eduardo@Eduardos-MacBook-Pro.local>
2018-10-17 11:45:53 -0400

    process env?

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def get_all_theaters():
 port = int(os.environ.get('PORT', 8080))
 
 if __name__ == '__main__':
-	app.run(debug = True, port =port )
+	app.run(debug = True, ALLOWED_HOSTS = ['127.0.0.1', 'crowdymovies.herokuapp.com'] )
